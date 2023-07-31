7e86b49be256d12d484fe9fe3b2661536a69a982
flying0307 <happywoo@gmail.com>
2023-07-31 14:41:33 +0800

    Support test in docker container.

--- src/load-env.ts
+++ src/load-env.ts
@@ -1,6 +1,11 @@
 
 import { config as dotenvConfig } from 'dotenv';
 import { join } from 'path';
-const envFile = process.env.NODE_ENV === 'dev' ? '.env.development' : '.env';
-console.log(envFile);
-dotenvConfig({ path: join(__dirname, '..', envFile) });
+if (process.env.NODE_ENV != 'docker') {
+  const envFile = process.env.NODE_ENV === 'dev' ? '.env.development' : '.env';
+  console.log(envFile);
+  dotenvConfig({ path: join(__dirname, '..', envFile) });
+} else {
+  console.log('Run in docker!');
+  dotenvConfig({ path: join(__dirname, '..', '.env.docker') });
+}
